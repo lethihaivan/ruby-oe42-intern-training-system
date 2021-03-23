6c137492cfcf25e53d42c32f1f3bbb45fecb03e7
lethihaivan <haivan17tclc2@gmail.com>
2021-03-23 17:04:45 +0700

    Init and config project

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,18 @@
+module ApplicationHelper
+  def full_title page_title = ""
+    base_title = t "base_title"
+    page_title.blank? ? base_title : "#{page_title} | #{base_title}"
+  end
+
+  def custom_bootstrap_flash
+    flash_messages = []
+    flash.each do |type, message|
+      type = "success" if type == "notice"
+      type = "error"   if type == "alert"
+      type = "warning" if type == "danger"
+      text = "<script>toastr.#{type}('#{message}');</script>"
+      flash_messages << text.html_escape if message
+    end
+    flash_messages.join("\n").html_escape
+  end
+end
---
+++ app/javascript/packs/application.js
@@ -0,0 +1,37 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+import "bootstrap"
+require("jquery")
+import I18n from "i18n-js/index.js.erb"
+import toastr from "toastr/toastr"
+
+toastr.options = {
+  "escapeHtml": true,
+  "closeButton": false,
+  "debug": false,
+  "newestOnTop": false,
+  "progressBar": false,
+  "positionClass": "toast-top-right",
+  "preventDuplicates": false,
+  "onclick": null,
+  "showDuration": "300",
+  "hideDuration": "1000",
+  "timeOut": "3000",
+  "extendedTimeOut": "1000",
+  "showEasing": "swing",
+  "hideEasing": "linear",
+  "showMethod": "fadeIn",
+  "hideMethod": "fadeOut"
+}
+window.I18n = I18n
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
+global.toastr = toastr
