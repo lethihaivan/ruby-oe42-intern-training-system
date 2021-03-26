d7edaa73475d9dd08537c3f21f533da456f2cc53
lethihaivan <haivan17tclc2@gmail.com>
2021-03-26 07:58:57 +0700

    Static_Page and Layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
+    root "static_pages#home"
+    get "/help", to: "static_pages#help"
     get "/login", to: "sessions#new"
     post "/login", to: "sessions#create"
-    get "/logout", to: "sessions#destroy"
-    resources :users, only: [:show]
+    delete "/logout", to: "sessions#destroy"
+    resources :users, only: :show
   end
 end
