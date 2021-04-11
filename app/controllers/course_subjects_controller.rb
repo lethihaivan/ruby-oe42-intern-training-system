3c53be9eedcf49374473a1912d193d413f903138
lethihaivan <haivan17tclc2@gmail.com>
2021-04-11 22:49:01 +0700

    View detail subject of course

---
+++ app/controllers/course_subjects_controller.rb
@@ -0,0 +1,25 @@
+class CourseSubjectsController < ApplicationController
+  before_action :load_course_subject, :load_subject, :load_tasks, :load_courses
+
+  def show; end
+
+  private
+
+  def load_course_subject
+    @course_subject = CourseSubject.find_by id: params[:id]
+  end
+
+  def load_subject
+    @subject = @course_subject.subject
+  end
+
+  def load_tasks
+    @tasks = @subject.tasks.newest.paginate page: params[:page],
+      per_page: Settings.course.paginate.per_page
+  end
+
+  def load_courses
+    @courses = @subject.courses.paginate page: params[:page],
+      per_page: Settings.course.paginate.per_page
+  end
+end
