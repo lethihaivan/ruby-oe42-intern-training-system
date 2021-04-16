ed8f9438f65dba998c8229e09785d4a70f88c9f3
lethihaivan <haivan17tclc2@gmail.com>
2021-04-16 07:52:53 +0700

    Trainee finish subject

--- app/controllers/trainee/course_subjects_controller.rb
+++ app/controllers/trainee/course_subjects_controller.rb
@@ -1,7 +1,6 @@
 class Trainee::CourseSubjectsController < UsersController
   before_action :load_course_subject, :load_user_subject,
                 :load_user_tasks, :load_subject, :load_tasks, only: :show
-
   def show; end
 
   private
@@ -36,6 +35,6 @@ class Trainee::CourseSubjectsController < UsersController
   end
 
   def load_tasks
-    @report_tasks = @subject.tasks.not_exit_in_user_task @user_subject
+    @receive_tasks = @subject.tasks.not_exit_in_user_task @user_subject
   end
 end
